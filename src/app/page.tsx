32cd219f0f17cbecb4d4b8d928d64bdcc31b30f6
godzz733 <godzz733@naver.com>
2024-06-25 17:17:17 +0900

    Hotfix: public 폴더
    - public 폴더 copy 구문 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
+import { Box, Container, Grid, Typography, Paper } from "@mui/material";
 import Appbar from "../components/Appbar";
+import MainHeader from "../components/MainHeader";
 
 export default function Home() {
   return (
     <>
       <Appbar />
+      <MainHeader />
+      <Container maxWidth="lg"></Container>
     </>
   );
 }
